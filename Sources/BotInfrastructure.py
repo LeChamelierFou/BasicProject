3e3ee017c19c38f471b5599d4e1cadceee17bf36
SylvainC <sylvain.cadel@lilo.org>
2023-07-20 20:51:53 +0200

    Modify df into dictDf to manage multiple pairs

--- Sources/BotInfrastructure.py
+++ Sources/BotInfrastructure.py
@@ -14,7 +14,7 @@ def on_open(ws):
     print('open')
 
 def on_message(ws, message):
-    global df, in_position, buyorders, sellorders, confirm, init
+    global dictDf, in_position, buyorders, sellorders, confirm, init
     out = json.loads(message)
     #print(out)
 
@@ -23,63 +23,70 @@ def on_message(ws, message):
     if confirm:
         namePairs = out['topic'].split('.')[-1] + out['topic'].split('.')[1]
 
-        out = pd.DataFrame({'name': namePairs,
-                            'open': float(out['data'][0]['open']), 'close': float(out['data'][0]['close']),
+        out = pd.DataFrame({'open': float(out['data'][0]['open']), 'close': float(out['data'][0]['close']),
                             'high': float(out['data'][0]['high']), 'low': float(out['data'][0]['low']),
                             'volume': float(out['data'][0]['volume'])},
                            index=[pd.to_datetime(out['data'][0]['start'], unit='ms')])
-        df = pd.concat([df, out], axis=0)
-        print(df)
+
+        if namePairs not in dictDf.keys():
+            dictDf[namePairs] = pd.DataFrame()
+            in_position[namePairs] = False
+            buyorders[namePairs] = []
+            sellorders[namePairs] = []
+
+        dictDf[namePairs] = pd.concat([dictDf[namePairs], out], axis=0)
+        print(dictDf[namePairs])
 
         # Manage CSV file
         csv_object = open(csvPath, 'a', newline='')
         writer_object = writer(csv_object)
 
+        # Faire en sorte de n'y rentrer qu'une fois!!!!!
         if init:
-            header = ['Action', 'Profit']
-            header.extend(df.columns.values.tolist())
+            header = ['Name', 'Action', 'Profit']
+            header.extend(dictDf[namePairs].columns.values.tolist())
             print(header)
             writer_object.writerow(header)
             init = False
 
         # Strategy on SMA on 5 elements
-        df = df.tail(5)  # 5 is a value of the value to keep in the dataframe for the calculation
-        last_price = df.tail(1).close.values[0]
-        sma_5 = df.close.rolling(5).mean().tail(1).values[0]
+        dictDf[namePairs] = dictDf[namePairs].tail(5)  # 5 is a value of the value to keep in the dataframe for the calculation
+        last_price = dictDf[namePairs].tail(1).close.values[0]
+        sma_5 = dictDf[namePairs].close.rolling(5).mean().tail(1).values[0]
 
+        # Verify if the buyorders, sellorders and in_position works
         # Buy signal
-        if not in_position and last_price > sma_5:
+        if not in_position[namePairs] and last_price > sma_5:
             # Manage values
-            buyorders.append(last_price)
-            in_position = True
+            buyorders[namePairs].append(last_price)
+            in_position[namePairs] = True
             #if canTradeWithBybit:
                 #Implemnt buying API + Verification of the remaining money
 
             # Display results
-            print('bought for ' + str(last_price))
+            print('bought' + namePairs + ' for ' + str(last_price))
 
             # Add a row in csv
-            row = ['Buy', 0]
-            row.extend(df.tail(1).values[0])
+            row = [namePairs, 'Buy', 0]
+            row.extend(dictDf[namePairs].tail(1).values[0])
             writer_object.writerow(row)
 
-
         # Sell signal
-        if in_position and sma_5 > last_price:
+        if in_position[namePairs] and sma_5 > last_price:
             # Manage values
             profit = str(last_price - buyorders[-1])
-            sellorders.append(last_price)
-            in_position = False
+            sellorders[namePairs].append(last_price)
+            in_position[namePairs] = False
             #if canTradeWithBybit:
                 #Implement selling API + Verification of the remaining tokens
 
             # Display results
-            print('sold for ' + str(last_price))
+            print('sold' + namePairs + ' for ' + str(last_price))
             print('Profit : ' + profit)
 
             # Add a row in csv
-            row = ['Sell', profit]
-            row.extend(df.tail(1).values[0])
+            row = [namePairs, 'Sell', profit]
+            row.extend(dictDf[namePairs].tail(1).values[0])
             writer_object.writerow(row)
 
         # Close csv file
@@ -108,9 +115,9 @@ if __name__ == "__main__":
                           'id': 1})
 
     # Init lists and booleans for on_message callback
-    df = pd.DataFrame()
-    buyorders, sellorders = [], []
-    in_position = False
+    dictDf = dict()
+    buyorders, sellorders = dict(), dict()
+    in_position = dict()
     confirm = False
 
     # Recuperer toutes les datas
